7405899157abff1a7bd57fd131374afbd71096e6
farhanchunawala <farhanchunawala.fc@gmail.com>
2023-05-09 00:00:17 +0530

    customer list majority front end done

--- pages/customer-list.js
+++ pages/customer-list.js
@@ -11,58 +11,53 @@ export default function Home() {
 			<title>Customer list</title>
 		</Head>
 		<main className={`${styles.main}`}>
-			<div className={`${styles.box}`}>
-			    <label className={`${styles.label}`}>No.</label>
-				<label className={`${styles.label}`}>fatima</label>
-				<label className={`${styles.label}`}>furqan</label>
-				<label className={`${styles.label}`}>faizan</label>
-				<label className={`${styles.label}`}>farhan</label>
-				<label className={`${styles.label}`}>mummy</label>
-				<label className={`${styles.label}`}>abbu</label>
-				<label className={`${styles.label}`}>family</label>
-		    </div>
-				{/* <form className={`${styles.input}`}> */}
-				{/* <label className={`${styles.field}`} htmlFor="input-field"></label>
-				<input id="input-field" type="text" style={{ width: "100px", height: "40px" }} />
-				<label className={`${styles.field}`} htmlFor="input-field"></label>
-				<input id="input-field" type="text" style={{ width: "100px", height: "40px" }} />
-				<label className={`${styles.field}`} htmlFor="input-field"></label>
-				<input id="input-field" type="text" style={{ width: "100px", height: "40px" }} />
-				<label className={`${styles.field}`} htmlFor="input-field"></label>
-				<input id="input-field" type="text" style={{ width: "100px", height: "40px" }} />
-				<label className={`${styles.field}`} htmlFor="input-field"></label>
-				<input id="input-field" type="text" style={{ width: "100px", height: "40px" }} />
-				<label className={`${styles.field}`} htmlFor="input-field"></label>
-				<input id="input-field" type="text" style={{ width: "100px", height: "40px" }} />
-				<label className={`${styles.field}`} htmlFor="input-field"></label>
-				<input id="input-field" type="text" style={{ width: "100px", height: "40px" }} />
-				<label className={`${styles.field}`} htmlFor="input-field"></label>
-				<input id="input-field" type="text" style={{ width: "100px", height: "40px" }} /> */}
-				{/* </form> */}
-			<div className={`${styles.table}`}>
-				<p>hi</p>
-			</div> <br></br>
-			<div className={`${styles.table}`}>
-			    <p>hi</p>
-			</div> <br></br>
-			<div className={`${styles.table}`}>
-			    <p>hi</p>
-			</div> <br></br>
-			<div className={`${styles.table}`}>
-				<p>hi</p>
-			</div> <br></br>
-			<div className={`${styles.table}`}>
-				<p>hi</p>
-			</div> <br></br>
-			<div className={`${styles.table}`}>
-				<p>hi</p>
-			</div> <br></br>
-			<div className={`${styles.table}`}>
-				<p>hi</p>
-			</div> <br></br>
-			<div className={`${styles.table}`}>
-				<p>hi</p>
-			</div> <br></br>
+			<div className={`${styles.list}`}>
+				<div className={`${styles.box}`}>
+					<label className={`${styles.label}`}>No.</label>
+					<label className={`${styles.label}`}>fatima</label>
+					<label className={`${styles.label}`}>furqan</label>
+					<label className={`${styles.label}`}>faizan</label>
+					<label className={`${styles.label}`}>farhan</label>
+					<label className={`${styles.label}`}>mummy</label>
+					<label className={`${styles.label}`}>abbu</label>
+					<label className={`${styles.label}`}>family</label>
+				</div>
+				<div className={`${styles.container}`}>
+					<div className={`${styles.table}`}>
+						<div className={`${styles.text}`}>
+							01
+							Furqan Chunawala
+							7738245067
+							Farhan Chunawala
+							Faizan Chunawala
+							kurta salwar
+							100,000
+							20%
+						</div>
+					</div> <br></br>
+					<div className={`${styles.table}`}>
+						1234567890
+					</div> <br></br>
+					<div className={`${styles.table}`}>
+						1234567890
+					</div> <br></br>
+					<div className={`${styles.table}`}>
+						1234567890
+					</div> <br></br>
+					<div className={`${styles.table}`}>
+						1234567890
+					</div> <br></br>
+					<div className={`${styles.table}`}>
+						1234567890
+					</div> <br></br>
+					<div className={`${styles.table}`}>
+						1234567890
+					</div> <br></br>
+					<div className={`${styles.table}`}>
+						1234567890
+					</div> <br></br>
+				</div>
+			</div>	
 		</main>
 		</>
 	)
