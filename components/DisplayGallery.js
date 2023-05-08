fdeda11bc9d90558135bf6d63eab14b17a62e863
farhanchunawala <farhanchunawala.fc@gmail.com>
2023-05-09 01:07:42 +0530

    sass changes

--- components/DisplayGallery.js
+++ components/DisplayGallery.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/DisplayGallery.module.css'
+import styles from '../styles/DisplayGallery.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
  
