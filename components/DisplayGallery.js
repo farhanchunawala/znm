d49413afd7a09ada2dbffa119fdd8222c4597478
cancermitrofficial <tech@cancermitr.com>
2023-05-18 01:01:05 +0530

    display gallery

--- components/DisplayGallery.js
+++ components/DisplayGallery.js
@@ -7,38 +7,38 @@ const Navbar = () => {
     return (
         <>
 		    <div className={`${styles.main}`}>
-				<h2 className={`${styles.content} ${styles.text}`}>MENSWEAR 23</h2>
-				<div className={`${styles.section1} ${styles.row}`}>
-					<div className={`${styles.col}`}>
-						<Image id="img1" className={`${styles.img}`} src="/img1.webp" alt="cloth" width="205" height="286"/>
+				<h2>MENSWEAR 23</h2>
+				<div className={`${styles.container}`}>
+					<div className={`${styles.frame}`}>
+						<Image id="img1" src="/img1.webp" alt="cloth" width="205" height="286"/>
 							{/* <p className={`${styles.imgtext}`}>Morning Mist</p> */}
 					</div>
-					<div className={`${styles.col}`}>
-						<Image id="img2" className={`${styles.img}`} src="/img2.webp" alt="cloth" width="205" height="286"/>
+					<div className={`${styles.frame}`}>
+						<Image id="img2" src="/img2.webp" alt="cloth" width="205" height="286"/>
 							{/* <p className={`${styles.imgtext}`}>pine Green</p> */}
 					</div>
-					<div className={`${styles.col}`}>
-						<Image id="img3" className={`${styles.img}`} src="/img3.webp" alt="cloth" width="205" height="286"/>
+					<div className={`${styles.frame}`}>
+						<Image id="img3" src="/img3.webp" alt="cloth" width="205" height="286"/>
 							{/* <p className={`${styles.imgtext}`}>Seafoam Green</p> */}
 					</div>		
-					<div className={`${styles.col}`}>
-						<Image id="img4" className={`${styles.img}`} src="/img4.webp" alt="cloth" width="205" height="286"/>
+					<div className={`${styles.frame}`}>
+						<Image id="img4" src="/img4.webp" alt="cloth" width="205" height="286"/>
 							{/* <p className={`${styles.imgtext}`}>Dusty Yale</p> */}
 					</div>
-					<div className={`${styles.col}`}>
-						<Image id="img5" className={`${styles.img}`} src="/img5.webp" alt="cloth" width="205" height="286"/>
+					<div className={`${styles.frame}`}>
+						<Image id="img5" src="/img5.webp" alt="cloth" width="205" height="286"/>
 							{/* <p className={`${styles.imgtext}`}>Sterling Palms</p> */}
 					</div>
-					<div className={`${styles.col}`}>
-						<Image id="img6" className={`${styles.img}`} src="/img6.webp" alt="cloth" width="205" height="286"/>
+					<div className={`${styles.frame}`}>
+						<Image id="img6" src="/img6.webp" alt="cloth" width="205" height="286"/>
 							{/* <p className={`${styles.imgtext}`}>Vintage Cobalt</p> */}
 					</div>   
-					<div className={`${styles.col}`}>
-						<Image id="img7" className={`${styles.img}`} src="/img7.jpg"  alt="cloth" width="205" height="286"/>
+					<div className={`${styles.frame}`}>
+						<Image id="img7" src="/img7.jpg"  alt="cloth" width="205" height="286"/>
 							{/* <p className={`${styles.imgtext}`}>Castlaton Shine</p> */}
 					</div>
-					<div className={`${styles.col}`}>
-						<Image id="img8" className={`${styles.img}`} src="/img8.webp" alt="cloth" width="205" height="286"/>
+					<div className={`${styles.frame}`}>
+						<Image id="img8" src="/img8.webp" alt="cloth" width="205" height="286"/>
 							{/* <p className={`${styles.imgtext}`}>Chanterelle beige</p> */}
 					</div>
 						{/* <button className={`${styles.viewall}`} type="button" > VIEW ALL </button> */}
